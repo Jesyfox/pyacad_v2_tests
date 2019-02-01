be014f4ac10f933f9e6680d4de53b379ab53336f
jesyfox <jesyfoxbs@gmail.com>
2019-02-01 23:36:23 +0200

    feature: added sumple models

--- pyFormers/tests/models.py
+++ pyFormers/tests/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Questions(models.Model):
+    category = models.CharField(max_length=100)
+    question_text = models.CharField(max_length=300)
+
+
+class Answers(models.Model):
+    question_id = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    answer_text = models.TextField()
