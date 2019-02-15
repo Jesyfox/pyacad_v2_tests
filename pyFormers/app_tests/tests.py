10319e3357ad711acc50aaf232d5e5a94668b151
jesyfox <jesyfoxbs@gmail.com>
2019-02-15 16:02:11 +0200

    feature: started making tests

--- pyFormers/app_tests/tests.py
+++ pyFormers/app_tests/tests.py
@@ -1,2 +1,18 @@
 from django.test import TestCase
+from .models import Test
+
+
+class CreateTestCase(TestCase):
+    test_name = 'My_testing_test'
+    question1 = 'what is your name?'
+    question2 = 'what is your favorite color?'
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.test = Test.objects.create(name=cls.test_name)
+
+
+class RunTestCase(TestCase):
+    pass
 
