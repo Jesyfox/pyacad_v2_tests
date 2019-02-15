789b2de4a68b172aa15c6af34f09991a26a1c2b5
jesyfox <jesyfoxbs@gmail.com>
2019-02-15 22:03:28 +0200

    feature: added add quest test

--- pyFormers/app_tests/tests.py
+++ pyFormers/app_tests/tests.py
@@ -1,8 +1,7 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.shortcuts import reverse
 from .models import Test, Question
-from .views import
-from django.db.models import Count
+from .views import add_q
 
 
 class CreateTestCase(TestCase):
@@ -14,12 +13,19 @@ class CreateTestCase(TestCase):
         cls.test = Test.objects.create(name=cls.test_name)
 
     def test_add_question(self):
-        q1 = Question.objects.create(text='what is your name?')
-        add_quest_url = reverse('add_q')
-        # request = self.client.post(add_quest_url)
-        self.assertEqual(Test.objects.values_list('questions'), 1)
+        q1 = Question(text='what is your name?')
+        q1.save()
+        adding_question = {'test_id': self.test.id, 'q_id': q1.id}
+        r = reverse('app_tests:add_q', kwargs=adding_question)
+        request = self.client.post(r, data=adding_question)
+        self.assertEqual(Test.objects.values_list('questions', flat=True).count(), 1)
 
+        q2 = Question(text='what is your favorite color?')
+        q2.save()
+        adding_question = {'test_id': self.test.id, 'q_id': q2.id}
+        r = reverse('app_tests:add_q', kwargs=adding_question)
+        request = self.client.post(r, data=adding_question)
+        self.assertEqual(Test.objects.values_list('questions', flat=True).count(), 2)
 
-# class RunTestCase(TestCase):
-#     pass
-
+    def test_run_test(self):
+        pass
