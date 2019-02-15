4e6527cbee5586a6fc0468f5ce3b9afa3a14d9cb
jesyfox <jesyfoxbs@gmail.com>
2019-02-15 22:45:10 +0200

    feature added run test test

--- pyFormers/app_tests/tests.py
+++ pyFormers/app_tests/tests.py
@@ -1,7 +1,8 @@
-from django.test import TestCase, Client
+from django.test import TestCase
 from django.shortcuts import reverse
-from .models import Test, Question
-from .views import add_q
+from .models import Test, Question, RunTest
+from .forms import AnswerForm
+from django.forms.models import formset_factory
 
 
 class CreateTestCase(TestCase):
@@ -28,4 +29,28 @@ class CreateTestCase(TestCase):
         self.assertEqual(Test.objects.values_list('questions', flat=True).count(), 2)
 
     def test_run_test(self):
-        pass
+        answer_factory = formset_factory(AnswerForm, min_num=2)
+        runing_test_1 = {'form-TOTAL_FORMS': '3',
+                         'form-INITIAL_FORMS': '0',
+                         'form-MIN_NUM_FORMS': '2',
+                         'form-MAX_NUM_FORMS': '1000',
+                         'form-0-Answer': 'King Arthur',
+                         'form-1-Answer': 'Blue',
+                         'start': 'Done'}
+        answer_form_set = answer_factory(runing_test_1)
+        self.assertTrue(answer_form_set.is_valid())
+
+        self.assertEquals(RunTest.objects.all().count(), 0)
+        r = reverse('app_tests:run_test', kwargs={'test_id': self.test.id})
+        request = self.client.post(r, data=runing_test_1)
+        self.assertEquals(RunTest.objects.all().count(), 1)
+
+        runing_test_2 = {'form-TOTAL_FORMS': '3',
+                         'form-INITIAL_FORMS': '0',
+                         'form-MIN_NUM_FORMS': '2',
+                         'form-MAX_NUM_FORMS': '1000',
+                         'form-0-Answer': '',
+                         'form-1-Answer': '',
+                         'start': 'Done'}
+        answer_form_set = answer_factory(runing_test_2)
+        self.assertFalse(answer_form_set.is_valid())
